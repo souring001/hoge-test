f62fe003bf64afa238b35f373ad25244e721e983
aso <titechei@gmail.com>
2022-01-23 19:33:26 +0900

    add search3

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const 星座リスト = ['おひつじ座', 'おうし座', 'ふたご座', 'かに座', 'しし座', 'おとめ座', 'てんびん座', 'さそり座', 'いて座', 'やぎ座', 'みずがめ座', 'うお座'];
-const 性別リスト = ['男', '女'];
 let 美川さん;
+
+const 星座リスト = ['おひつじ座', 'おうし座', 'ふたご座', 'かに座', 
+                  'しし座', 'おとめ座', 'てんびん座', 'さそり座', 
+                  'いて座', 'やぎ座', 'みずがめ座', 'うお座'];
+const 性別リスト = ['男', '女'];
 
 class Mikawa {
   constructor(星座, 性別) {
@@ -31,6 +34,27 @@ class Mikawa {
       return false;
     }
   }
+
+  isGender(性別) {
+    if (this.性別 == 性別) {
+      console.log('(頷く)');
+      return true;
+    } else {
+      console.log('(首を振る)');
+      return false;
+    }
+  }
+
+  isZodiacSign(星座) {
+    if (this.星座 == 星座) {
+      console.log('(頷く)');
+      return true;
+    } else {
+      console.log('(首を振る)');
+      return false;
+    }
+  }
+
 }
 
 function init() {
@@ -51,8 +75,44 @@ function search2() {
   }
 }
 
+function search3() {
+  let 美川さんの星座;
+  let 美川さんの性別;
+
+  for (const 星座 of 星座リスト) {
+    console.log(星座+'ですか？');
+    if (美川さん.isZodiacSign(星座)) {
+      美川さんの星座 = 星座;
+      break;
+    }
+  }
+
+  for (const 性別 of 性別リスト) {
+    console.log(性別+'ですか？');
+    if (美川さん.isGender(性別)) {
+      美川さんの性別 = 性別;
+      break;
+    }
+  }
+
+  console.log('ってことは'+美川さんの星座+'の'+美川さんの性別+'ですね？');
+  美川さん.isAsked(美川さんの星座, 美川さんの性別);
+}
+
+function search2() {
+  for (const 星座 of 星座リスト) {
+    for (const 性別 of 性別リスト) {
+      console.log(星座+'の'+性別+'ですか？');
+      let 返事 = 美川さん.isAsked(星座, 性別);
+      if (返事) {
+        return;
+      }
+    }
+  }
+}
+
 init();
-search2();
+search3();
 美川さん.startSinging();
 
 
